97e11a08b82a3e9676462623c19b9a93b033fdc2
unknown <ahlxt@qq.com>
2016-06-13 00:23:14 +0800

    the pub account

---
+++ src/gochatserver/controllers/pub.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"gochatserver/models"
+	gutils "gochatserver/utils"
+	"encoding/xml"
+	"time"
+	"os"
+	"log"
+	"fmt"
+
+)
+
+type PubController struct  {
+	beego.Controller
+}
+
+func (c * PubController) Get() {
+
+}
+
+func (c * PubController) Post() {
+
+}
